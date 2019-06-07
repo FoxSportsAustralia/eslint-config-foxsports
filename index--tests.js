e0100a9f0dbba5a2094f26b3c3a16cd61d331fec
Ming Teo <ming.teo@foxsports.com.au>
2019-06-07 08:08:57 +0800

    MWL-877 - add new file for importing testing standards

---
+++ index--tests.js
@@ -0,0 +1,6 @@
+/**
+ * @fileoverview Web developer javascript test standards for foxsports &amp; kayo
+ */
+'use strict';
+
+module.exports = require('./src/configs/browserify-mocha-sinon-chai.js');
