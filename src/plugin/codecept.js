39c8d5580dd0a406b4889aafd672ae68917866be
Maria Vargas <maria.vargas@streamotion.com.au>
2021-11-11 19:29:40 +1100

    - update codecept config

--- src/plugin/codecept.js
+++ src/plugin/codecept.js
@@ -6,10 +6,13 @@
  */
 module.exports = {
     plugins: ['codeceptjs'],
+    env: {
+        'codeceptjs/codeceptjs': true,
+    },
     rules: {
         'codeceptjs/no-actor-in-scenario': 'off',
         'codeceptjs/no-exclusive-tests': 'error',
-        'codeceptjs/no-skipped-tests': 'error',
+        'codeceptjs/no-skipped-tests': 'off',
         'codeceptjs/no-pause-in-scenario': 'error',
     },
 };
